57544e9f56c6b02b5233abc925b1ed022e0600f5
Achyut <achyut1608@gmail.com>
2020-06-09 16:33:04 +0530

    Update ProducerConsumerUsingBlockingQueue.java

--- JavaMultithreading/src/UsingRunnableInterface/ProducerConsumerUsingBlockingQueue.java
+++ JavaMultithreading/src/UsingRunnableInterface/ProducerConsumerUsingBlockingQueue.java
@@ -2,22 +2,49 @@ package UsingRunnableInterface;
 
 public class ProducerConsumerUsingBlockingQueue {
 	public static void main(String[]args) {
-		
+		BlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
+		Thread producer = new Thread(new Producer(queue));
+		Thread consumer = new Thread(new Consumer(queue));
+		producer.start();
+		consumer.start();
 	}
 }
 
 
 class ProducerBlockingQueueThread implements Runnable {
-	
+	BlockingQueue<Integer> queue;
+	ProducerBlockingQueueThread(BlockingQueue<Integer> queue){
+		this.queue = queue;
+	}
 	@Override
 	public void run() {
-		
+		for(int i=0;i<10;i++){
+			try {
+				queue.put(i);
+				System.out.println("Added successfully");
+				Thread.sleep(1000);
+			} catch(InterruptedException ie){
+				ie.printStackTrace();
+			}
+		}
 	}
 }
 
 class ConsumerBlockingQueueThread implements Runnable {
+	BlockingQueue<Integer> queue;
+	
+	ConsumerBlockingQueueThread(BlockingQueue<Integer> queue){
+		this.queue = queue;
+	}
 	@Override
 	public void run() {
-		
+		try {
+			while(true){
+				int item = queue.take();
+				System.out.println("item :"+item);
+			}
+		} catch(InterruptedException ie){
+			ie.printStackTrace();
+		}
 	}
 }
